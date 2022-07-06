d87cab161435d6d8c8a212580ddf441982d2d551
ひろろ <hrsi.ad@gmail.com>
2022-07-06 14:30:09 +0900

    会員登録画面デザイン　完了

--- LoginWithFirebaseApp/ViewController.swift
+++ LoginWithFirebaseApp/ViewController.swift
@@ -9,11 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emsilTextField: UITextField!
+
+    @IBOutlet weak var passwordTextField: UITextField!
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var registerButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        registerButton.layer.cornerRadius = 10
+
+        registerButton.isEnabled = false
+        registerButton.backgroundColor = UIColor.rgb(red: 255, green: 221, blue: 187)
+
+
+        emsilTextField.delegate = self
+        passwordTextField.delegate = self
+        usernameTextField.delegate = self
+
+
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
 
 
 }
 
+extension ViewController: UITextFieldDelegate {
+
+
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        let emailIsEmpty = emsilTextField.text?.isEmpty ?? true
+        let passwordIsEmpty = passwordTextField.text?.isEmpty ?? true
+        let usernameIsEmpty = usernameTextField.text?.isEmpty ?? true
+
+        if emailIsEmpty || passwordIsEmpty || usernameIsEmpty {
+            registerButton.isEnabled = false
+            registerButton.backgroundColor = UIColor.rgb(red: 255, green: 221, blue: 187)
+        } else {
+            registerButton.isEnabled = true
+            registerButton.backgroundColor = UIColor.rgb(red: 255, green: 141, blue: 0)
+        }
+
+
+        print(":", textField.text)
+    }
+}
